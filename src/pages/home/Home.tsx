9b2bccb94effc343d296bc702a25d1b31e99bbd4
Daniel Lima <dsl.daniel.santos.lima@gmail.com>
2025-02-03 18:16:09 -0300

    home atualizada + perfil

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import ModalExercicio from "../../components/exercicio/modalpostagem/ModalExercicio";
-import ListaExercicio from "../../components/exercicio/listaexercicio/ListaExercicio";
 
 function Home() {
   const [hovered, setHovered] = useState(false);
@@ -9,27 +8,34 @@ function Home() {
     <>
       <div
         className="relative w-full min-h-screen bg-fixed bg-cover bg-center"
-        style={{ backgroundImage: "url('https://i.postimg.cc/QMvT0m2G/imagemdefundo.jpg')" }}
+        style={{ backgroundImage: "url('https://i.postimg.cc/hP5hvrS6/imagemdefundo2.jpg')" }}
       >
-        <main className="relative flex justify-center items-center min-h-screen">
-          <div className="container text-center text-white">
-            <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className="text-5xl font-bold text-yellow-400 drop-shadow-md">
-                FIT, o seu aplicativo fitness
+        <main className="relative flex flex-col justify-center items-center min-h-screen gap-8">
+          <div className="container flex flex-col items-center justify-center text-white">
+            <div className="flex flex-col items-center mb-4">
+              <div className="rounded-lg shadow-lg overflow-hidden">
+                <img
+                  src="https://i.postimg.cc/1XghjDLV/iconecomfundo.png"
+                  alt="Logo"
+                  style={{ width: "100px" }}
+                  className="rounded-md"
+                />
+              </div>
+              <h2 className="text-5xl font-bold text-yellow-400 drop-shadow-md text-center mt-2 shadow-lg">
+                Cardanis, o seu aplicativo fitness!
               </h2>
-              <p className="text-xl text-gray-200 drop-shadow-sm">
+            </div>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <p className="text-xl text-gray-200 drop-shadow-sm text-center shadow-lg">
                 Criar um treino personalizado nunca foi tão fácil!
               </p>
-
-              <div className="flex justify-around gap-4">
+              <div className= "flex justify-around gap-4">
                 <ModalExercicio />
               </div>
             </div>
           </div>
         </main>
       </div>
-
-      <ListaExercicio />
     </>
   );
 }
